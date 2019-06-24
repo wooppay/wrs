1a933830eec1179598a4b5a1e1b22cb8228ef51f
Yuriy Sirotenko <spider4216@gmail.com>
2019-06-24 15:17:04 +0600

    slick fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+    slickInit();
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        $(".slider").slick("unslick");
+        slickInit();
+    });
+});
+
+function slickInit() {
     $('.slider').slick({
         draggable: false,
         infinite: false,
         slidesPerRow: 3,
-        slidesToShow: 3,
+        slidesToShow: 3
     });
-});
+
+}
