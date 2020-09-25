a908ab133cb0548359eae43c86b06537c73bebb5
Даниил Кучкин <dkushkin@wooppay.com>
2020-09-25 15:09:46 +0600

    WRS-108 fix: fix permissions

--- src/Form/UserReportType.php
+++ src/Form/UserReportType.php
@@ -32,7 +32,7 @@ class UserReportType extends AbstractType
     	//TODO: Do lazy load
     	if (isset($options['userService'])) {
 		    $optionsForChoiceField['choice_loader'] = new CallbackChoiceLoader(function () use ($options) {
-			    return $options['userService']->allByRoleName(RoleEnum::DEVELOPER);
+			    return $options['userService']->all();
 		    });
 	    }
 
