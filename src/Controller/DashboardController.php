3e6729a145d56d0bfca7c4c2bcafd7964781ac0f
Даниил Кучкин <dkushkin@wooppay.com>
2020-10-26 12:25:07 +0600

    WRS-106 fix: optimize get tasks, remove unnecessary AJAX request

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,39 +2,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Security;
-use App\Enum\PermissionEnum;
 use App\Service\TaskService;
-use App\Service\RateInfoService;
 use App\Service\UserService;
 use App\Service\TeamService;
 use App\Entity\Task;
 use App\Entity\Project;
 use App\Entity\Team;
 use App\Form\TaskType;
-use App\Enum\RoleEnum;
 use App\Form\ProjectType;
 use App\Form\TeamType;
 use App\Service\RoleService;
 
 class DashboardController extends AbstractController
 {
-    public function main(RoleService $roleService, Security $security, TaskService $taskService, RateInfoService $rateInfoService, UserService $userService, TeamService $teamService)
+    public function main(RoleService $roleService, TaskService $taskService, UserService $userService, TeamService $teamService)
     {
         $user = $this->getUser();
+
         $tasks = $taskService->tasksForDashboardByUser($user);
-        $receiveMarks = count($user->getRates());
-        $authorMarks = count($user->getAuthorRates());
+
+        $receiveMarks = $user->getRates()->count();
+        $authorMarks = $user->getAuthorRates()->count();
+
         $tasksUsers = $userService->allApprovedExceptAdminAndOwnerAndCustomer();
         $taskForm = $this->createForm(TaskType::class, (new Task()), [
             'users' => $tasksUsers,
         ]);
+
         $projectCustomers = $userService->allByRoleName($roleService->getRoleCustomer()->getName());
         $teams = $teamService->all();
         $projectForm = $this->createForm(ProjectType::class, (new Project()), [
             'teams' => $teams,
             'customers' => $projectCustomers,
         ]);
+
         $teamForm = $this->createForm(TeamType::class, (new Team()));
 
         return $this->render('dashboard/main.html.twig', [
