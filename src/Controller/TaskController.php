c71d7cff35ba9aced099a6989faa3549b9e3487a
Даниил Кучкин <dkushkin@wooppay.com>
2020-11-02 14:27:03 +0600

    WRS-118 fix: fix denied archive task, if it was marked

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -112,12 +112,12 @@ class TaskController extends AbstractController
             throw $this->createNotFoundException('Task does not exist!');
         }
 
+	    if ($task->getAuthor() != $this->getUser()) {
+		    throw $this->createAccessDeniedException();
+	    }
+
         if (!$taskService->isAuthor($this->getUser(), $task)) {
             throw $this->createNotFoundException('You are not the author of this task');
-        }
-      
-        if ($taskService->isTaskMarked($task)) {
-            throw $this->createNotFoundException('This task has already marked');
         }
 
         if ($task->getStatus() == TaskEnum::DELETED) {
